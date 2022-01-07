5d0c806cf1a0ec159d8c80baccd614515c63f4a0
Brett Zamir <brettz9@yahoo.com>
2022-01-07 17:36:35 +0800

    test: begin Mocha tests (`getLicenseType`, `satisfies`)

---
+++ test/getLicenseType.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const getLicenseType = require('../lib/getLicenseType');
+
+describe('`getLicenseType`', function () {
+  it('Gets "uncategorized" if unrecognized', function () {
+    expect(getLicenseType('blah')).to.equal('uncategorized');
+  });
+
+  it('Gets license type (UNLICENSED as uncategorized)', function () {
+    expect(getLicenseType('UNLICENSED')).to.equal('uncategorized');
+  });
+
+  it('Gets license type (protective)', function () {
+    expect(getLicenseType('GPL-3.0')).to.equal('protective');
+  });
+
+  it('Gets license type (permissive)', function () {
+    expect(getLicenseType('MIT')).to.equal('permissive');
+  });
+});
