321984941318211a0f6b7e6001e4e55084f8e18e
Brett Zamir <brettz9@yahoo.com>
2022-01-07 18:28:46 +0800

    test: some testing of `walkDependencies`

---
+++ test/walkDependencies.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const walkDependencies = require('../lib/walkDependencies');
+
+describe('`walkDependencies`', function () {
+  it('Walks dependencies', async function () {
+    this.timeout(30000);
+    expect(await walkDependencies({argparse: '1.0.0'})).to.deep.equal({
+      'argparse@1.0.0': {
+        dependencies: {
+          lodash: '^3.2.0',
+          'sprintf-js': '~1.0.2'
+        },
+        license: 'MIT',
+        licenseType: 'permissive',
+        modified: '2015-02-19T07:23:51.940Z',
+        name: 'argparse',
+        size: null,
+        version: '1.0.0',
+        versionLoose: '1.0.0'
+      },
+      'lodash@3.10.1': {
+        dependencies: {},
+        license: 'MIT',
+        licenseType: 'permissive',
+        modified: '2015-08-04T06:05:06.887Z',
+        name: 'lodash',
+        size: null,
+        version: '3.10.1',
+        versionLoose: '^3.2.0'
+      },
+      'sprintf-js@1.0.3': {
+        dependencies: {},
+        license: 'BSD-3-Clause',
+        licenseType: 'permissive',
+        modified: '2015-07-10T13:41:29.308Z',
+        name: 'sprintf-js',
+        size: null,
+        version: '1.0.3',
+        versionLoose: '~1.0.2'
+      }
+    });
+  });
+
+  /*
+  it.only('Walks dependencies', async function () {
+    this.timeout(30000);
+    expect(await walkDependencies({'a@bad@package': '1.0.0'})).to.deep.equal({
+    });
+  });
+  */
+});
