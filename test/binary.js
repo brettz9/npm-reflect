ac81249b0669cdd2e8ee3bb9ca3067280f66871a
Brett Zamir <brettz9@yahoo.com>
2022-01-07 20:31:21 +0800

    test: binary file

---
+++ test/binary.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const {promisify} = require('util');
+const {execFile: ef} = require('child_process');
+
+const execFile = promisify(ef);
+const binFile = './bin/index.js';
+
+
+describe('Binary', function () {
+  this.timeout(8000);
+  it('should log help', async function () {
+    const {stdout} = await execFile(binFile, ['-h']);
+    expect(stdout).to.contain(
+      'Check npm package dependencies'
+    );
+  });
+});
