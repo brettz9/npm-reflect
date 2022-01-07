8a98a3f3c682516b029f89c7501f546bc8cace44
Brett Zamir <brettz9@yahoo.com>
2022-01-07 22:39:23 +0800

    test: check `isProduction`

---
+++ test/isProduction.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const isProduction = require('../lib/isProduction');
+
+describe('`isProduction`', function () {
+  it('Checks if is production (false)', async function () {
+    const isProd = await isProduction({});
+    expect(isProd).to.equal(false);
+  });
+
+  it('Checks if is production', async function () {
+    const isProd = await isProduction({production: true});
+    expect(isProd).to.equal(true);
+  });
+});
