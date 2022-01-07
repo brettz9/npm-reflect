1e9ca7b514540d3f8c5f3d48861ce750f80463a7
Brett Zamir <brettz9@yahoo.com>
2022-01-07 17:45:00 +0800

    test: check `getLicenseStr`

---
+++ test/getLicenseStr.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const getLicenseStr = require('../lib/getLicenseStr');
+
+describe('`getLicenseStr`', function () {
+  it('Gets string if supplied a string', function () {
+    expect(getLicenseStr('MIT')).to.equal('MIT');
+  });
+
+  it('Gets "Unknown" if supplied an object without a type', function () {
+    expect(getLicenseStr({})).to.equal('Unknown');
+    expect(getLicenseStr({type: ''})).to.equal('Unknown');
+  });
+
+  it('Gets "Unknown" if supplied falsey value without a type', function () {
+    expect(getLicenseStr(null)).to.equal('Unknown');
+  });
+
+  it('Gets type if supplied an object with a type', function () {
+    expect(getLicenseStr({type: 'MIT'})).to.equal('MIT');
+  });
+
+  it('Gets OR type if supplied an array of types', function () {
+    expect(getLicenseStr([{type: 'MIT'}, 'GPL-3.0'])).to.equal('(MIT OR GPL-3.0)');
+  });
+});
