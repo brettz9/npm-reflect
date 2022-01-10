88e1af914e6e36243a6d13cc59b0a61cf69655c1
Brett Zamir <brettz9@yahoo.com>
2022-01-10 20:59:35 +0800

    test: check `testPackageManager` methods

---
+++ test/testPackageManager.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const {join} = require('path');
+
+const {testYarn, testPnpm} = require('../lib/testPackageManager');
+
+const cwd = process.cwd();
+
+describe('testPackageManager', function () {
+  afterEach(() => {
+    process.chdir(cwd);
+  });
+  describe('`testYarn`', function () {
+    it('Finds yarn accurately', async function () {
+      process.chdir(join(__dirname, 'fixtures/yarn-path'));
+      expect(await testYarn()).to.equal(true);
+    });
+    it('Reports false if yarn missing', async function () {
+      process.chdir(join(__dirname, 'fixtures/pnpm-path'));
+      expect(await testYarn()).to.equal(false);
+    });
+  });
+  describe('`testPnpm`', function () {
+    it('Finds pnpm accurately', async function () {
+      process.chdir(join(__dirname, 'fixtures/pnpm-path'));
+      expect(await testPnpm()).to.equal(true);
+    });
+    it('Reports false if pnpm missing', async function () {
+      process.chdir(join(__dirname, 'fixtures/yarn-path'));
+      expect(await testPnpm()).to.equal(false);
+    });
+  });
+});
