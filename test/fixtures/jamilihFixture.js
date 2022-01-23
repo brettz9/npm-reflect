39c6be00d82cbe75b37176deb843d932f9081f0b
Brett Zamir <brettz9@yahoo.com>
2022-01-23 19:16:10 +0800

    test: complete `getImpact` coverage

---
+++ test/fixtures/jamilihFixture.js
@@ -0,0 +1,14 @@
+const jamilihFixture = {
+  'jamilih@0.54.0': {
+    name: 'jamilih',
+    modified: '2021-02-21T10:04:02.470Z',
+    version: '0.54.0',
+    license: 'MIT',
+    licenseType: 'permissive',
+    dependencies: {},
+    versionLoose: '^0.54.0',
+    size: null
+  }
+};
+
+export default jamilihFixture;
--- test/getImpact.js
+++ test/getImpact.js
@@ -3,6 +3,7 @@ import {join, dirname} from 'path';
 
 import getImpact from '../lib/getImpact.js';
 import spdxCorrectFixture from './fixtures/spdxCorrectFixture.js';
+import jamilihFixture from './fixtures/jamilihFixture.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const cwd = process.cwd();
@@ -12,7 +13,7 @@ describe('`getImpact`', function () {
   afterEach(() => {
     process.chdir(cwd);
   });
-  it('Returns info table', async function () {
+  it('Returns info table with dependencies with new licenses', async function () {
     process.chdir(join(__dirname, 'fixtures/npm-path'));
 
     const impact = await getImpact({}, spdxCorrectFixture);
@@ -27,5 +28,78 @@ Licenses${space}${brightBlackFG}${space}${defaultFG}Apache-2.0${space}${brightBl
 ${space.repeat(9)}${brightBlackFG} ${defaultFG}CC-BY-3.0${space.repeat(2)}${brightBlackFG}${space}${defaultFG}1${space.repeat(8)}
 ${space.repeat(9)}${brightBlackFG} ${defaultFG}CC0-1.0${space.repeat(4)}${brightBlackFG}${space}${defaultFG}1${space.repeat(8)}`
     );
+  });
+
+  it('Returns info table with saveDev: true and new licenses', async function () {
+    process.chdir(join(__dirname, 'fixtures/devDeps-only-path'));
+
+    const impact = await getImpact({
+      saveDev: true
+    }, spdxCorrectFixture);
+    const brightBlackFG = '\u001B[90m';
+    const defaultFG = '\u001B[39m';
+    const space = '\u0020';
+
+    expect(impact).to.equal(
+      `Packages${space.repeat(1)}${brightBlackFG} ${defaultFG}4${space.repeat(10)}${brightBlackFG} ${defaultFG}+400.00%${space}
+Size${space.repeat(5)}${brightBlackFG} ${defaultFG}0 B${space.repeat(8)}${brightBlackFG} ${defaultFG}+NaN%${space.repeat(4)}
+Licenses${space}${brightBlackFG}${space}${defaultFG}Apache-2.0${space}${brightBlackFG}${space}${defaultFG}1${space.repeat(8)}
+${space.repeat(9)}${brightBlackFG} ${defaultFG}CC-BY-3.0${space.repeat(2)}${brightBlackFG}${space}${defaultFG}1${space.repeat(8)}
+${space.repeat(9)}${brightBlackFG} ${defaultFG}CC0-1.0${space.repeat(4)}${brightBlackFG}${space}${defaultFG}1${space.repeat(8)}`
+    );
+  });
+
+  it('Indicates no new licenses when none new supplied', async function () {
+    process.chdir(join(__dirname, 'fixtures/npm-path'));
+
+    const impact = await getImpact({}, jamilihFixture);
+    const brightBlackFG = '\u001B[90m';
+    const defaultFG = '\u001B[39m';
+    const space = '\u0020';
+
+    expect(impact).to.equal(
+      `Packages${space.repeat(1)}${brightBlackFG} ${defaultFG}0${space.repeat(3)}${brightBlackFG} ${defaultFG}+0.00%${space}
+Size${space.repeat(5)}${brightBlackFG} ${defaultFG}0 B${space}${brightBlackFG} ${defaultFG}+NaN%${space.repeat(2)}
+No new licenses${space.repeat(7)}`
+    );
+  });
+
+  it('Throws upon missing dependencies', async function () {
+    process.chdir(join(__dirname, 'fixtures/missing-deps-path'));
+    let error;
+    try {
+      await getImpact({}, spdxCorrectFixture);
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).to.be.an('Error');
+    expect(error.message).to.equal('Local package has no dependencies');
+  });
+
+  it('Throws upon missing dependencies and unused devDependencies', async function () {
+    process.chdir(join(__dirname, 'fixtures/devDeps-only-path'));
+    let error;
+    try {
+      await getImpact({}, spdxCorrectFixture);
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).to.be.an('Error');
+    expect(error.message).to.equal('Local package has no dependencies');
+  });
+
+  it('Throws upon empty dependencies', async function () {
+    process.chdir(join(__dirname, 'fixtures/empty-deps-path'));
+    let error;
+    try {
+      await getImpact({}, spdxCorrectFixture);
+    } catch (err) {
+      error = err;
+    }
+
+    expect(error).to.be.an('Error');
+    expect(error.message).to.equal('Local package has no dependencies');
   });
 });
