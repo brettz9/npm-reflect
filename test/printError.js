c44014b3fbfe5ee35e9fb5e8ae9946b8ea6d183b
Brett Zamir <brettz9@yahoo.com>
2022-01-26 16:00:50 +0800

    testi: fix to reset spy

--- test/printError.js
+++ test/printError.js
@@ -1,8 +1,15 @@
 import printError from '../lib/printError.js';
 
+const {error} = console;
+
 describe('`printError`', function () {
+  after(() => {
+    // eslint-disable-next-line no-console -- Spy
+    console.error = error;
+  });
   it('Prints a colored string if supplied a string', function (done) {
-    global.console.error = (str) => {
+    // eslint-disable-next-line no-console -- Spy
+    console.error = (str) => {
       expect(str).to.equal('\u001B[31mTest\u001B[39m');
       done();
     };
